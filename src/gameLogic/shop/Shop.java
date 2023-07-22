db59ba81344b25ac222f307a091614f1e090d858
Daniel <102766299+Daniel-Loader@users.noreply.github.com>
2023-07-23 01:13:38 +1200

    Update Shop.java

--- src/gameLogic/shop/Shop.java
+++ src/gameLogic/shop/Shop.java
@@ -1,12 +1,35 @@
 package gameLogic.shop;
 
+import gameLogic.monsters.Monster;
+import gameLogic.items.Item;
+
 import static java.lang.Math.pow;
 
 public class Shop {
-    public static int getPurchasePrice(int rarity, int level) {
-        return (int) (10 * pow((rarity + 1), 2) * pow(level, 2));
+    private Static List<Shop> instances = new ArrayList<Shop>();
+    private Static List<Monster> allMonsters = Monster.getAll();
+    private Static List<Item> items = Item.getAll();
+
+    private final String[] description;
+    private final List<Monster> monsters = new ArrayList<Item>();
+    private final List<Item> items = new ArrayList<Item>();
+
+    /**
+      *returns the cost to buy a monster or item of the given level and rarity.
+      *
+      * 
+      *
+      */
+    private static int getPurchasePrice(int rarity, int level) {
+        return (int) (10 * pow((level + 1), rarity));
     }
 
+    public Shop(String inside, String outside);
+
+    public void offerWares() {
+        System.out.Println(description[2])
+        
+    }
     /* TODO
     *   purchasing
     *   selling
