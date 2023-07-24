380abf989b32716383a9456c94aa90a06387ea7f
Daniel <102766299+Daniel-Loader@users.noreply.github.com>
2023-07-24 13:29:41 +1200

    Update AI.java
    With a Easter egg to implement later.

--- src/gameLogic/battle/AI.java
+++ src/gameLogic/battle/AI.java
@@ -4,3 +4,107 @@ public class AI {
 
     // This should just be random
 }
+
+/** my tribute to chatGBT */
+Sure! Below is an implementation of the Manic Manor game using Java. This implementation includes the `ManicManor` class, `Player` class, `Cell` class, and `EstateCell` class. Note that this is a simplified version of the game for demonstration purposes.
+
+```java
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents the main class for the Manic Manor game.
+ */
+public class ManicManor {
+    private int rows;
+    private int columns;
+    private Cell[][] board;
+    private List<Player> players;
+
+    /**
+     * Creates a new ManicManor game with the specified number of rows and columns.
+     *
+     * @param rows    The number of rows on the game board.
+     * @param columns The number of columns on the game board.
+     */
+    public ManicManor(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.board = new Cell[rows][columns];
+        this.players = new ArrayList<>();
+        initializeBoard();
+    }
+
+    /**
+     * Initializes the game board with EstateCells and sets starting positions for players.
+     */
+    private void initializeBoard() {
+        // TODO: Implement logic to initialize the board with EstateCells
+        // and set starting positions for players.
+    }
+
+    /**
+     * Adds a player to the game.
+     *
+     * @param player The player to add to the game.
+     */
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    /**
+     * Moves the specified player to a new position on the game board.
+     *
+     * @param player The player to move.
+     * @param row    The new row for the player's position.
+     * @param column The new column for the player's position.
+     */
+    public void movePlayer(Player player, int row, int column) {
+        // TODO: Implement logic to move the player to the specified position on the board.
+    }
+
+    /**
+     * Represents a player in the game.
+     */
+    public static class Player {
+        private String name;
+        private int row;
+        private int column;
+
+        public Player(String name) {
+            this.name = name;
+        }
+
+        // Getters and setters for row, column, and name
+        // ...
+
+        /**
+         * Checks if the player is occupying the given cell.
+         *
+         * @param cell The cell to check.
+         * @return true if the player is occupying the cell, false otherwise.
+         */
+        public boolean isOccupying(Cell cell) {
+            // TODO: Implement logic to check if the player is occupying the given cell.
+            return false;
+        }
+    }
+
+    /**
+     * Represents a cell on the game board.
+     */
+    public static class Cell {
+        // TODO: Add any relevant properties and methods for the cell.
+    }
+
+    /**
+     * Represents a cell on the game board that corresponds to an estate.
+     * This class is a subclass of Cell and inherits its properties and methods.
+     */
+    public static class EstateCell extends Cell {
+        // TODO: Add any relevant properties and methods for the estate cell.
+    }
+}
+```
+
+Please note that the implementation provided above contains placeholders (`TODO` comments) where you need to fill in the logic for initializing the game board, moving players, and handling other aspects of the game. Additionally, you may need to add more properties and methods to the classes to represent the full functionality of the Manic Manor game.
